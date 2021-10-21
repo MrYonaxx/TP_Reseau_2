84e3b3e0672c6f8ef765a7b3c5b6697ea5a746f8
David XIA <xia@et.esiea.fr>
2021-10-21 19:57:54 -0400

    ajout compressor integer

---
+++ TPReseau2/TPReseau2/IntCompressor.cpp
@@ -0,0 +1,27 @@
+#include "IntCompressor.h"
+
+namespace uqac::serializer
+{
+	
+	void IntCompressor::Compressor(Serializer s, int val)
+	{
+
+            maxRange = max - min;
+            
+            if (maxRange <= 255) {
+                uint8_t newval = val + maxRange;
+                s.Serialize(newval);
+            }
+            else if( 255 < maxRange <= 65535) {
+                uint16_t newval = val + maxRange;
+                s.Serialize(newval);
+            }
+            else if (65535 < maxRange <= 4294967295) {
+                uint32_t newval = val + maxRange;
+                s.Serialize(newval);
+            }
+            
+            
+
+	}
+}
---
+++ TPReseau2/TPReseau2/IntCompressor.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Serializer.h"
+
+namespace uqac::serializer
+{
+	class IntCompressor
+	{
+
+	private:
+
+		int min;
+		int max;
+		int maxRange;
+
+	public:
+
+		void Compressor(Serializer s, int val);
+		void UnCompression();
+
+
+	};
+}
